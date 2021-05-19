5bf2264c30dc5a068ac50273c972090160c3e979
{name} <leobuosi09@hotmail.com>
2021-05-19 00:42:26 -0300

    Ecommerce

---
+++ Controllers/CarrinhoController.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce2021a.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce2021a.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Item> lista = new List<Item>();
+
+            
+
+            return View();
+        }
+    }
+}
